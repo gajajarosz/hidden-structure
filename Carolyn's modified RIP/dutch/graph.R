84a869cbd6df9654dd77f73a3376b2f86c111c04
Carolyn Anderson <canders277@gmail.com>
2016-09-15 12:01:50 -0400

    deleting output files
    eventually i’ll write a gitignore so these aren’t tracked.

--- Carolyn's modified RIP/dutch/graph.R
+++
@@ -1,8 +0,0 @@
-library(ggplot2)
-args <- commandArgs(TRUE)
-srcFile <- args[1]
-name = paste(srcFile, ".csv", sep="")
-data <- read.csv(name)
-ggplot(data,aes(iterations,accuracy,color=syllable))+geom_line()+labs(x="Iterations",y="Percent accurate")+ggtitle(srcFile)
-graphname = paste(srcFile, ".pdf", sep="")
-ggsave(file=graphname)
